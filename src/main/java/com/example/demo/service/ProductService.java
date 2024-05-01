7a76f8b722c3f7f440b94abc8cde84524271d1d7
Ricardo Nascimento <ricardop794@gmail.com>
2024-05-01 12:48:45 -0300

    feat: Adicionar metodo para busca todas entidades relacionadas de uma só vez evitando o n + 1 consultas.

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -10,15 +10,20 @@ import com.example.demo.dto.ProductDTO;
 import com.example.demo.entities.Product;
 import com.example.demo.repositories.ProductRepository;
 
+import java.util.stream.Collectors;
+
 @Service
 public class ProductService {
-	
-	@Autowired
-	private ProductRepository repository;
 
-	@Transactional(readOnly = true)
-	public Page<ProductDTO> find(PageRequest pageRequest) {
-		Page<Product> list = repository.findAll(pageRequest);
-		return list.map(x -> new ProductDTO(x));
-	}
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> find(PageRequest pageRequest) {
+//        Aqui eu busco todas os produtos que foi especificado na paginação pelo size, porém por padrão é lazy loading
+        Page<Product> list = repository.findAll(pageRequest);
+//        Aqui eu faço um join fetch para trazer as categorias de cada produto de uma só vez evitando fazer varias consultas ao banco
+        repository.findProductsCategories(list.stream().collect(Collectors.toList()));
+        return list.map(ProductDTO::new);
+    }
 }
